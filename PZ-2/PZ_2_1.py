5e6ba92d3f1ea5fcdd3a209ffdafe9e1f0fd1992
Bob-05 <studentazizov@gmail.com>
2024-11-30 23:10:32 +0300

    Error correction

--- PZ-2/PZ_2_1.py
+++ PZ-2/PZ_2_1.py
@@ -1,15 +1,17 @@
 # Известно, что X кг конфет стоит A рублей;
 # Определить, сколько стоит 1 кг и Y кг этих же конфет.
 
-number_candies_old = int(input('Enter the number of candies: '))
-while type(number_candies_old) != int:
+weight_candies_old = int(input('Enter the weight of the candies in kilograms: '))
+while not(type(weight_candies_old) == int):
     try:
-        number_candies_old = int(number_candies_old)
+        weight_candies_old = int(weight_candies_old)
     except ValueError:
         print("Invalid input!!!")
-        number_candies_old = int(input('Enter an integer: '))
+        weight_candies_old = int(input('Enter an integer: '))
 
-cost_candy_old = float(input('Enter the total cost of candies:'))
+
+
+cost_candy_old = float(input('Enter the cost in rubles: '))
 while type(cost_candy_old) != int:
     try:
         cost_candy_old = int(cost_candy_old)
@@ -24,10 +26,10 @@ cost_one_candy = float(number_candies_old / cost_candy_old)
 number_candies_new = int(input('Enter the new number of candies: '))
 while type(number_candies_new) != int:
     try:
-        number_candies_new = int(number_candies_new)
+        weight_candies_new = int(weight_candies_new)
     except ValueError:
         print("Invalid input!!!")
-        number_candies_new = int(input('Enter an integer: '))
+        weight_candies_new = int(input('Enter an integer: '))
 
-cost_candy_new = number_candies_new + cost_one_candy
-print("Cost", number_candies_new, "of candy = ", cost_candy_new)
+cost_candy_new = weight_candies_new + cost_one_candy
+print("Cost", weight_candies_new, "a kilogram of sweets = ", cost_candy_new, "in rubles.")
