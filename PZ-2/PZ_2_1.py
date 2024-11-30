38e66ae7bb7564a78e5fdba56e05f8c1e0f7f523
Bob-05 <studentazizov@gmail.com>
2024-11-30 23:46:06 +0300

    Error correction

--- PZ-2/PZ_2_1.py
+++ PZ-2/PZ_2_1.py
@@ -1,35 +1,29 @@
 # Известно, что X кг конфет стоит A рублей;
 # Определить, сколько стоит 1 кг и Y кг этих же конфет.
 
-weight_candies_old = int(input('Enter the weight of the candies in kilograms: '))
-while not(type(weight_candies_old) == int):
-    try:
-        weight_candies_old = int(weight_candies_old)
-    except ValueError:
-        print("Invalid input!!!")
-        weight_candies_old = int(input('Enter an integer: '))
+def get_positive_int(promt):
+    while True:
+        try:
+            value = int(input(promt))
+            if value > 0:
+                return value
+            else:
+                print ("Enter a number greater than zero.")
+
+        except ValueError:
+            print("Incorrect input!!! Enter a positive integer")
 
 
-
+# Получаем данные от пользователя
+weight_candies_old = get_positive_int('Enter the weight of the candies in kilograms: ')
 cost_candy_old = float(input('Enter the cost in rubles: '))
-while type(cost_candy_old) != int:
-    try:
-        cost_candy_old = int(cost_candy_old)
-    except ValueError:
-        print("Invalid input!!!")
-        cost_candy_old = int(input('Enter an integer: '))
 
+# Рассчитываем стоимость одного килограмма конфет
+cost_one_kilogram = cost_candy_old / weight_candies_old
 
-# How much does one kg of sweets cost?
-cost_one_candy = float(number_candies_old / cost_candy_old)
+weight_candies_new = get_positive_int('Enter the new candy weight: ')
+cost_candy_new = weight_candies_new * cost_one_kilogram
 
-number_candies_new = int(input('Enter the new number of candies: '))
-while type(number_candies_new) != int:
-    try:
-        weight_candies_new = int(weight_candies_new)
-    except ValueError:
-        print("Invalid input!!!")
-        weight_candies_new = int(input('Enter an integer: '))
-
-cost_candy_new = weight_candies_new + cost_one_candy
-print("Cost", weight_candies_new, "a kilogram of sweets = ", cost_candy_new, "in rubles.")
+# Выводим результаты
+print("The cost of one kilogram of sweets =", cost_one_kilogram)
+print("Cost", weight_candies_new, "kg of sweets =", cost_candy_new, "in rubles.")
