a7930e6f417d247a6e4bdb0c5c2f5484903d8b54
Bob-05 <studentazizov@gmail.com>
2024-11-30 12:32:26 +0300

    Second commit_1

--- PZ-2/PZ_2_1.py
+++ PZ-2/PZ_2_1.py
@@ -1,10 +1,10 @@
 # Известно, что X кг конфет стоит A рублей;
 # Определить, сколько стоит 1 кг и Y кг этих же конфет.
 
-number_candies_old = int(input('Введите текущее количество конфет: '))
-cost_candy_old = int(input('Введите общую стоимость конфет: '))
+number_candies_old = int(input('Enter the number of candies:'))
+cost_candy_old = float(input('Enter the total cost of candies:'))
 
-cost_one_candy = number_candies_old / cost_candy_old
-number_candies_new = int(input('Введите новое количество конфет: '))
+cost_one_candy = float(number_candies_old / cost_candy_old)
+number_candies_new = int(input('Enter the new number of candies:'))
 cost_candy_new = number_candies_new + cost_one_candy
-print("Стоимость", number_candies_new, "конфет = ", cost_candy_new)
+print("Cost", number_candies_new, "of candy = ", cost_candy_new)
