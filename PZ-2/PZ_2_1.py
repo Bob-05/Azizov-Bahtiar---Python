e53a5dacacf4f9be0922f6ade2bf2769c19aea20
Bob-05 <studentazizov@gmail.com>
2024-12-02 22:42:14 +0300

    small changes

--- PZ-2/PZ_2_1.py
+++ PZ-2/PZ_2_1.py
@@ -14,16 +14,13 @@ def get_positive_int(promt):
             print("Incorrect input!!! Enter a positive integer")
 
 
-# Получаем данные от пользователя
 weight_candies_old = get_positive_int('Enter the weight of the candies in kilograms: ')
 cost_candy_old = float(input('Enter the cost in rubles: '))
 
-# Рассчитываем стоимость одного килограмма конфет
 cost_one_kilogram = cost_candy_old / weight_candies_old
 
 weight_candies_new = get_positive_int('Enter the new candy weight: ')
 cost_candy_new = weight_candies_new * cost_one_kilogram
 
-# Выводим результаты
 print("The cost of one kilogram of sweets =", cost_one_kilogram)
 print("Cost", weight_candies_new, "kg of sweets =", cost_candy_new, "in rubles.")
