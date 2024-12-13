aaf3d2ba843bda7426a2894cf97315c13276d07c
vicxre <kirskor1337@gmail.com>
2024-12-13 14:05:57 +0300

    Edit PZ-4_1

--- PZ-4/PZ-4_1.py
+++ PZ-4/PZ-4_1.py
@@ -1,17 +1,17 @@
 # Даны два целых числа A и B (A < B).
 # Вывести в порядке возрастания все целые числа,
-# расположенные между A и B (включая сами числа A и B),
-# а также количество этих чисел.
+# расположенные между A и B (включая сами числа A и B), а также количество этих чисел.
 
 def check(digit):
     value = input(digit)
 
-    while type(value) != float:
+    while type(value) != int:
         try:
-            value = float(value)
-            if (type(value) == float):
-                return value
-        except:
+            value = int(value)
+            if type(value) == int:
+                if value > 0:
+                    return value
+        except ValueError:
             print("Enter a digit!!!")
             value = input(digit)
 
@@ -24,3 +24,12 @@ while True:
     else:
         print('Error - incorrect input!!!')
 
+number_N = number_A
+
+while True:
+    print("--", number_N)
+    number_N += 1
+    if number_N > number_B:
+        break
+
+print("Number of whole numbers between", number_A, "and ", number_B, " = ", number_N)
