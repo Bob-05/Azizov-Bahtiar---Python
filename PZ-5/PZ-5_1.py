6f1d0bfd7171103e6cc78a40e6a4e500e884aa5a
dioloxmadarasolo <valerieivanova89@gmail.com>
2024-12-14 12:27:19 +0300

    //

--- PZ-5/PZ-5_1.py
+++ PZ-5/PZ-5_1.py
@@ -13,6 +13,20 @@ def check(type_string) -> object:
 
 
 user_digit = check('Enter digit: ')
-def aspiration_to_zero(user_dg): pass
+def aspiration_to_zero(user_dg):
+    counter = 0
+    new_digit = 0
+    while True:
+        number = 10
+        while user_dg % number >= 0 :
+            new_digit += new_digit % number
+            number *= 10
+            print("...")
+        user_dg = new_digit
+        new_digit = 0
+        print("...")
+        counter += 1
+        if user_dg % number < 0:
+            return counter
 
-
+print ("Number of actions = ", aspiration_to_zero(user_digit))
