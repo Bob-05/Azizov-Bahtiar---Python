108a58b1bf58973b4573166c36b6b94f2bfb0e56
Bob-05 <studentazizov@gmail.com>
2025-01-24 14:03:09 +0300

    edit pz-7_2

--- PZ-7/PZ-7_2.py
+++ PZ-7/PZ-7_2.py
@@ -5,7 +5,19 @@
 или номер позиции, в которой расположена первая ошибочная скобка, или, если
 закрывающих скобок не хватает, число —1
 """
+def check_for_my_symbol(a):
+    list_my_symbol = ['(',')','{','}','[',']']
+    list_user_symbol = str()
+    for i in a:
+        for j in list_my_symbol:
+            if i == j:
+                list_user_symbol += i
+
+    return list_user_symbol
 
 
-string_user = input("Введите текст: ")
-for elem in string_user:
+
+string_user = str(input("Введите текст: "))
+print(type(string_user), " " , check_for_my_symbol(string_user))
+
+#for elem in string_user:
