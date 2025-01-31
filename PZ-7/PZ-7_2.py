aa4068cb8a93f4cd214a9b34e4c9a9c0bd77e622
Bob-05 <studentazizov@gmail.com>
2025-01-31 12:29:48 +0300

    edit pz-7_2

--- PZ-7/PZ-7_2.py
+++ PZ-7/PZ-7_2.py
@@ -8,24 +8,20 @@
 
 
 def check_parentheses(s):
-    open_brackets = "([{"
-    close_brackets = ")]}"
-    stack = ""
+    # Стек для хранения открывающих скобок
+    stack = []
 
-    for index in range(len(s)):
-        char = s[index]
-        if char in open_brackets:
-            stack += char
-        elif char in close_brackets:
+    for index, char in enumerate(s):
+        if char in '([{':
+            stack.append((char, index + 1))
+        elif char in ')]}':
+            # Если стек пуст или верхний элемент стека не соответствует закрывающей скобке
             if not stack:
                 return index + 1
-            last_open = stack[-1]
-            if (last_open == '(' and char == ')') or (last_open == '[' and char == ']') or (last_open == '{' and char == '}'):
-                stack = stack[:-1]
-        else:
-            return index + 1
-
-
+            top_char, top_index = stack.pop()
+            if (char == ')' and top_char != '(') or (char == ']' and top_char != '[') or (char == '}' and top_char != '{'):
+                return index + 1
+    # недостаток элементов
     if stack:
         return -1
     # всё верно
