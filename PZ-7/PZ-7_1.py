91a7112ef44396236bae4435b5bf349d054dfa33
Bob-05 <studentazizov@gmail.com>
2025-01-31 12:23:19 +0300

    edit pz-7_1

--- PZ-7/PZ-7_1.py
+++ PZ-7/PZ-7_1.py
@@ -1,10 +1,10 @@
 # Дан символ C.
 # Вывести его код (то есть номер в кодовой таблице).
 
-symbol_user = input("Введите символ: ")
+symbol_user = input("Enter the symbol(s): ")
 
 if len(symbol_user) > 1:
-    print("символ - код")
+    print("symbol - cod")
     for i in range(len(symbol_user)):
         print(f"    {symbol_user[i]} -> {ord(symbol_user[i])}")
 else:
