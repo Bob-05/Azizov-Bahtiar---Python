9c2fd407b77a11e2a4ba3a17927767a9dbe01f1f
bahtiarazizov95@gmail.com <bahtiarazizov95@gmail.com>
2025-06-05 21:07:43 +0300

    update PZ-15

--- PZ-15/PZ-15_1.py
+++ PZ-15/PZ-15_1.py
@@ -26,14 +26,28 @@ with sq.connect('base.db') as con:
                       (2, 'Кефир', 'Пятёрочка', 900, 2809, 'литр', '110руб/литр'),
                       (3, 'Кола', 'Магнит', 1321, 1409, 'литр', '190руб/литр'),
                       (4, 'Вода питьевая', 'Лента', 895, 205, 'литр', '20руб/литр'),
-                      (5, 'Вода газированая', 'Лента', 893, 207, 'литр', '27руб/литр'),
+                      (5, 'Вода газированная', 'Лента', 893, 207, 'литр', '27руб/литр'),
+                      (6, 'Хлеб белый', 'Пятёрочка', 456, 1500, 'штука', '45руб/штука'),
+                      (7, 'Яйца', 'Магнит', 789, 3000, 'десяток', '95руб/десяток'),
+                      (8, 'Сахар', 'Лента', 321, 1200, 'кг', '65руб/кг'),
+                      (9, 'Соль', 'Пятёрочка', 654, 1800, 'кг', '30руб/кг'),
+                      (10, 'Масло подсолнечное', 'Магнит', 987, 900, 'литр', '150руб/литр')
                       ]
-    cur.executemany("INSERT INTO services VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", info_for_table)
+    cur.executemany("INSERT INTO items VALUES (?, ?, ?, ?, ?, ?, ?)", info_for_table)
 
-    cur.execute("SELECT * FROM items")
-    # print(cur.fetchall())
-    # cur.execute("SELECT * FROM ")
-    # print(cur.fetchall())
-    # cur.execute("SELECT * FROM ")
-    # print(cur.fetchall())
+    #cur.execute("SELECT * FROM items WHERE name_shop = 'Пятёрочка'")
+    #print(cur.fetchall())
+    #cur.execute("SELECT * FROM items WHERE CAST(SUBSTR(trade_price, 1, INSTR(trade_price, 'руб')-1) AS INTEGER) > 100")
+    #print(cur.fetchall())
+    #cur.execute("SELECT * FROM items WHERE unit_measure = 'литр' AND number_item > 2000")
+    #print(cur.fetchall())
 
+    #cur.execute("DELETE FROM items WHERE order_shop < 500")
+    #cur.execute("DELETE FROM items WHERE name_shop = 'Магнит'")
+    #cur.execute("DELETE FROM items WHERE number_item < 300")
+
+    #cur.execute("UPDATE items SET trade_price = '130руб/литр' WHERE name_item = 'Молоко'")
+    #cur.execute("UPDATE items SET number_item = number_item + 100 WHERE name_item = 'Вода питьевая'")
+    #cur.execute("UPDATE items SET name_shop = 'Ашан' WHERE order_shop BETWEEN 800 AND 900")
+
+    print(cur.rowcount)
